64daa810e1042d5a88fcd39c3ff2362805afd64a
Guilherme Inoe <guilhermeinoe82@gmail.com>
2025-10-14 22:01:56 -0300

    feat: convertendo para cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char** argv){
     }
 
     VM vm;
-    VM_Inicializar(&vm, 0x200);
+    vm.VM_Inicializar(&vm, 0x200);
     
-    VM_CarregarROM(&vm, argv[1], 0x200);
+    vm.VM_CarregarROM(&vm, argv[1], 0x200);
 
     #ifdef DEBUG
-    VM_ImprimirRegistradores(&vm);
+    vm.VM_ImprimirRegistradores(&vm);
     #endif
 
     while(1){
-        VM_ExecutarInstrucao(&vm);
+        vm.VM_ExecutarInstrucao(&vm);
         #ifdef DEBUG
-        VM_ImprimirRegistradores(&vm);
+        vm.VM_ImprimirRegistradores(&vm);
         #endif
     }
 }
